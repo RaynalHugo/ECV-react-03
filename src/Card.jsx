182a5fd91a30ad497807709d21237129ee9a7f62
OuahElPato <115996568+OuahElPato@users.noreply.github.com>
2022-12-15 12:41:38 +0100

    Add backgroundcolor

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,6 +2,25 @@ import { useState, useEffect } from 'react'
 import styles from './Card.module.css'
 //console.log(styles.card); récupérer le nom d'une class module
 
+const typeColor = {
+  bug: "#26de81",
+  dragon: "#ffeaa7",
+  electric: "#fed330",
+  fairy: "#FF0069",
+  fighting: "#30336b",
+  fire: "#f0932b",
+  flying: "#81ecec",
+  grass: "#00b894",
+  ground: "#EFB549",
+  ghost: "#a55eea",
+  ice: "#74b9ff",
+  normal: "#95afc0",
+  poison: "#6c5ce7",
+  psychic: "#a29bfe",
+  rock: "#2d3436",
+  water: "#0190FF",
+};
+
 export function Card({ name, url }) {
   const [pokemon, setPokemon] = useState()
 
@@ -23,8 +42,8 @@ export function Card({ name, url }) {
   const firstType = types[0].type.name;
   console.log(firstType);
 
-  return (<div className={`${styles.card} ${styles[firstType]}`}>
+  return (<div className={styles.card} style={{backgroundColor:typeColor[firstType]}}>
     <h2>{pokemon.name}</h2>
-    <img src={pokemon.sprites.front_default} alt="image de pikachu" />
+    <img src={pokemon.sprites.other.dream_world.front_default} alt="image de pikachu" />
   </div>);
 }
