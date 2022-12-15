242a02ae83ae0525b0f4668804699644a812e384
OuahElPato <115996568+OuahElPato@users.noreply.github.com>
2022-12-15 10:03:52 +0100

    First Call with Pokemon API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState()
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/pikachu/")
+        .then(response => response.json())
+        .then(result => setSelectedPokemon(result))
+  },[])
 
   return (
-    <div className="App">Hello World !</div>
+    <div className="App">
+      <pre>{JSON.stringify(selectedPokemon, null, 2)}</pre>
+    </div>
   )
 }
 
