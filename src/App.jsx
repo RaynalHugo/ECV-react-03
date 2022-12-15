1e367ac2885842a664db44f920a13dbf8e4e2a22
OuahElPato <115996568+OuahElPato@users.noreply.github.com>
2022-12-15 12:32:14 +0100

    Add search funtion

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
+import {useState} from 'react'
+import {useEffect} from 'react'
 import './App.css'
-import { Card } from './Card'
+import {Card} from './Card'
 
 // Requête vers API + affichage composant Card
 function App() {
-  const [pokemons, setPokemons] = useState()
-  const [inputName, setInputName] = useState('')
-  console.log(pokemons)
+    const [pokemons, setPokemons] = useState()
+    const [inputName, setInputName] = useState('')
+    console.log(pokemons)
 
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon-species?offset=0&limit=1000')
-    .then(response => response.json())
-    .then(result => setPokemons(result.results))
-  }, [])
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon-species?offset=0&limit=1000')
+            .then(response => response.json())
+            .then(result => setPokemons(result.results))
+    }, [])
 
-  return (
-    <div className="App">
-      <input type="text" value={inputName} onChange={(event) => {
-        setInputName(event.target.value)
-      }} />
-      {pokemons !==undefined ? <div className={"wrapper"}>{pokemons.map((pokemon) => {
-        return <Card name={pokemon.name} url={pokemon.url} />
-      })}</div> : 'loading...' }
-    </div>
-  )
+    return (
+        <div className="App">
+            <input type="text" value={inputName} onChange={(event) => {
+                setInputName(event.target.value)
+            }}/>
+            {pokemons !== undefined ? <div
+                className={"wrapper"}>{pokemons.filter((pokemon) => pokemon.name.startsWith(inputName.toLowerCase())).map((pokemon) => {
+                return <Card name={pokemon.name} url={pokemon.url}/>
+            })}</div> : 'loading...'}
+        </div>
+    )
 }
 
 export default App
